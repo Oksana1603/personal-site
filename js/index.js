7ca96931c64ebbfd7c5275425fb0147bc563e073
Oksana <oksana16zahorodniuk@gmail.com>
2018-09-15 11:28:55 +0000

    blog

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-const form= document.getElementsByClassName('was-validated');
-const radio = document.getElementsByClassName('custom-control-input');
-const wrong = document.getElementById('wrong');
-const correct = document.getElementById('correct');
-console.log(radio);
-if (wrong) {console.log(wrong);
+const form = document.getElementById('testForm');
+
+
+//console.log(radio);
+//if (wrong) {console.log(wrong);}
+//if(correct) {console.log(correct)};
+const testBtn = document.getElementById('testBtn');
+let RandomTest=document.getElementById('RandomTest1');
+const alertText =document.getElementsByClassName("alert alert-dismissible alert-danger");
+
+testBtn.addEventListener('click',()=>{
+   console.log(form.value);
+   //if(form.value=='wrong')
+   {
+//   RandomTest.appendChild(alertText);
+  
+          
     
 }
+
+           
+});
