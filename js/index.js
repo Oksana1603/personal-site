b8a172aa5ee7e3a0bd5294dee8f3652d234c25e9
Oksana <oksana16zahorodniuk@gmail.com>
2018-09-16 19:00:59 +0000

    evening

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
-const form = document.getElementById('testForm');
+const form = document.getElementsByClassName('was-validated');
+const answer=document.getElementById('answer');
 
-
-//console.log(radio);
-//if (wrong) {console.log(wrong);}
-//if(correct) {console.log(correct)};
 const testBtn = document.getElementById('testBtn');
 let RandomTest=document.getElementById('RandomTest1');
 const alertText =document.getElementsByClassName("alert alert-dismissible alert-danger");
 
+const wrong = `<div class="alert alert-dismissible alert-danger">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+</div>`;
+var div = document.createElement('div');
+div.appendChild(document.createTextNode('top div'));
+
+
 testBtn.addEventListener('click',()=>{
-   console.log(form.value);
-   //if(form.value=='wrong')
+   console.log(answer.value);
+   switch (answer.value) {
+      case 'wet':
+               break;
+      case 'dry':
+      console.log(wrong); 
+      alert(wrong)
+ //const alertElement = $('body').append(wrong);
+   //   alertElement.alert();
+         
+      default:
+         // code
+   }
+   
+   
+ 
+   
+   
+   
    {
 //   RandomTest.appendChild(alertText);
   
